4ecfd87e90c34c395ec043b1b1f8f243df3954a9
theodo <36780272@qq.com>
2017-12-18 20:50:20 +0800

    代码结构调整

--- jeefast-rest/src/main/java/cn/jeefast/config/MybatisPlusConfig.java
+++ jeefast-rest/src/main/java/cn/jeefast/config/MybatisPlusConfig.java
@@ -5,19 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
-//import com.baomidou.mybatisplus.plugins.PerformanceInterceptor;
 
 @Configuration
 @MapperScan(basePackages = {"cn.jeefast.*.dao"})
 public class MybatisPlusConfig {
-
-    /**
-     * mybatis-plus SQL执行效率插件【生产环境可以关闭】
-     */
-    /*@Bean
-    public PerformanceInterceptor performanceInterceptor() {
-        return new PerformanceInterceptor();
-    }*/
 
     /**
      * mybatis-plus分页插件<br>
